ab2cd76864a2ef095113ef7bb9ea29c56e237665
Andrii Novakovskyi <141185356+oniguy4649@users.noreply.github.com>
2024-10-05 20:38:15 +0300

    Change website name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "oniguy4649's Chiebukuro";
+export const SITE_TITLE = 'Chiebukuro';
 export const SITE_DESCRIPTION = 'Blog of oniguy4649!';
